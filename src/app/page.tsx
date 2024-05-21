7bdf5ff01c7f2750acc3b450373114356c02ac52
Jonas Baptista Franco <jonas.franco@gan.com.br>
2024-05-21 16:49:12 -0300

    segundo commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+"use client"
+
+import { Sidebar } from "@/components/Sidebar";
+import { useState } from "react";
+
 const Page = () => {
+  const [sidebarOpened, setSidebarOpened] = useState(true);
+
+  const closeSidebar = () => {
+    setSidebarOpened(false);
+  }
+
   return (
     <main className="flex min-h-screen bg-gpt-gray">
+      <Sidebar
+        open={sidebarOpened}
+        onClose={closeSidebar}
+      >
 
+      </Sidebar>
+
+      <section className="flex flex-col w-full">
+        <button onClick={() => setSidebarOpened(true)}>Abrir Sidebar</button>
+      </section>
     </main>
   );
 }
